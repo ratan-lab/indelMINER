feefef4afd89d09b240df23cc82975bdae6cb8dc
Joao Rietra <jhlrietra@gmail.com>
2015-06-27 20:19:12 -0300

    Update and rename superfasthash.h to hashfunc.h

---
+++ src/hashfunc.h
@@ -0,0 +1,9 @@
+#ifndef _HASHFUNC_H_
+#define _HASHFUNC_H_
+
+#include <stdlib.h>
+#include <stdint.h>
+
+uint32_t hashfunc(const char* data, int len);
+
+#endif
--- src/superfasthash.h
+++
@@ -1,9 +0,0 @@
-#ifndef SUPERFASTHASH_H
-#define SUPERFASTHASH_H
-
-#include <stdlib.h>
-#include <stdint.h>
-
-uint32_t superfasthash (const char* data, int len);
-
-#endif
